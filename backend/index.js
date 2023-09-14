d62538f8717c7212ecc8263507f406cddb390006
Junaid Mohiyuddin <75275830+Mr-JM316@users.noreply.github.com>
2023-09-14 19:48:49 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express =  require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv=require("dotenv").config()
+const Stripe = require('stripe')
 
 const app = express()
 app.use(cors())
@@ -19,16 +20,16 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("Connect to Datab
 
 //schema
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
+    firstName:String,
+    lastName:String,
     email:{
-        type: String,
+        type:String,
         unique: true,
 
     },
-    password: String,
-    confirmPassword: String,
-    image: String,
+    password:String,
+    confirmPassword:String,
+    image:String,
 
 })
 
@@ -39,51 +40,50 @@ app.get("/",(req,res)=>{
 })
 
 //signup
-app.post("/signup",async(req,res)=>{
+app.post("/signup",async (req,res)=>{
     console.log(req.body)
-    const {email}=req.body
-
-    // userModel.findOne({email : email},(err,result)=>{
-    //     console.log(result)
-    //     console.log(err)
-    //     if(email===email){
-    //       res.send({message:"Email id is already register"})
-    //    }
-    //    else{
-    //      const data=userModel(req.body)
-    //       const save = data.save()
+   //const {email}=req.body
+    //userModel.findOne({email : email},(err,result)=>{
+       // console.log(result)
+       // console.log(err)
+        //if(result){
+          //res.send({message:"Email id is already register"})
+     //  }
+       //else{
+        //const data=userModel(req.body)
+          //const save = data.save()
            res.send({message:"Successfully Sign Up",alert:true})
         //}
-   //  })
+   // })
 })
 
 //api login
 app.post("/login",(req,res)=>{
-    console.log(req.body)
+    //console.log(req.body)
   //const {email}= req.body
    //userModel.findOne({email : email},(err,result)=>{
-       // if(result){
-          //  console.log(result)
-           // console.log(err)
-        //    const dataSend={
-        //         _id:result._id,
-        //         firstName: result.firstName,
-        //         lastName: result.lastName,
-        //       email: result.email,
-        //        password: result.password,
-        //         confirmPassword: result.confirmPassword,
-        //         image: result.image,
+    //    if(result){
+    //        console.log(result)
+    //        console.log(err)
+    //        const dataSend={
+    //             _id:result._id,
+    //             firstName: result.firstName,
+    //             lastName: result.lastName,
+    //           email: result.email,
+    //            password: result.password,
+    //             confirmPassword: result.confirmPassword,
+    //             image: result.image,
 
-        //     };
-           // console.log(dataSend)
+          //  };
+         //  console.log(dataSend)
              res.send({message:"Login is successfully",alert:true})
         
-       //}
-       //else{
-        //res.send({message:"Email is not available,please sign up",alert:false})
       // }
+       //else{
+       // res.send({message:"Email is not available,please sign up",alert:false})
+     // }
 
-   // })
+  // })
 })
 
 //product section
@@ -111,5 +111,52 @@ app.get("/product",async(req,res)=>{
     const data = await  productModel.find({})
     res.send(JSON.stringify(data))
 })
+
+console.log(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+//paymentgateway
+app.post("/checkout-payment",async(req,res)=>{
+    // console.log(req.body)
+    try{
+    const params = {
+        submit_type: 'pay',
+        mode:"payment",
+        payment_method_types : ['card'],
+        billing_address_collection :"auto",
+        shipping_options: [{shipping_rate:"shr_1NqFK1SG1hI6Hag1q8Yfop2G"}],
+        line_items: req.body.map((item)=>{
+            return{
+                price_data : {
+                    currency : "inr",
+                    product_data: {
+                        name :item.name,
+                        // images: [item.image]
+                    },
+                    unit_amount: item.price * 100,
+                },
+                adjustable_quantity: {
+                    enabled: true,
+                    minimum: 1,
+
+                },
+                quantity : item.qty
+            }
+        }),
+        success_url : `${process.env.FRONTEND_URL}/success`,
+        cancel_url :  `${process.env.FRONTEND_URL}/cancel`,
+
+    }
+
+    const session = await stripe.checkout.sessions.create(params)
+    res.status(200).json(session.id)
+}
+catch(err){
+    res.status(err.statusCode || 500).json(err.message)
+
+}
+
+})
+
+
 
 app.listen(PORT,()=>console.log("Server is running at Port:"+PORT))
